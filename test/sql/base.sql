0590189f9f3fa61189e625ea321243e3066e4792
Tomas Vondra <tomas@vondra.me>
2024-12-24 23:06:55 +0100

    Stamp 1.0.3

--- test/sql/base.sql
+++ test/sql/base.sql
@@ -5,6 +5,7 @@ SET client_min_messages = 'WARNING';
 \i sql/lower_quantile--1.0.0.sql
 \i sql/lower_quantile--1.0.0--1.0.1.sql
 \i sql/lower_quantile--1.0.1--1.0.2.sql
+\i sql/lower_quantile--1.0.2--1.0.3.sql
 SET client_min_messages = 'NOTICE';
 
 \set ECHO all
